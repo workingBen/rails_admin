f78a325da5f42b9c48d37bfce9fe9438ea568dca
Benoit Bénézech <benoit.benezech@gmail.com>
2011-06-30 15:24:32 +0200

    removed old deprecated features

--- lib/rails_admin/config/sections.rb
+++ lib/rails_admin/config/sections.rb
@@ -28,21 +28,6 @@ module RailsAdmin
             @sections[name].instance_eval &block if block
             @sections[name]
           end
-          # Register a shortcut to define the model's label for each section.
-          klass.send(:define_method, "label_for_#{name}") do
-            # TODO: Remove this warning in the next release, after people have updated their applications
-            $stderr.puts("WARNING: label_for_#{name} has been removed. This configuration will be ignored. Model names can be configured with 'label' and section-specific names are no longer supported. See README for details.")
-          end
-          # Register a shortcut to hide the model for each section.
-          klass.send(:define_method, "hide_from_#{name}") do |&block|
-            # TODO: Remove this warning in the next release, after people have updated their applications
-            $stderr.puts("WARNING: hide_from_#{name} has been removed. This configuration will be ignored. Model visibility can be configured with 'hide', 'show' and 'visibility'. Section-specific visibility is no longer supported. See README for details.")
-          end
-          # Register a shortcut to show the model for each section.
-          klass.send(:define_method, "show_in_#{name}") do |&block|
-            # TODO: Remove this warning in the next release, after people have updated their applications
-            $stderr.puts("WARNING: show_in_#{name} has been removed. This configuration will be ignored. Model visibility can be configured with 'hide', 'show' and 'visibility'. Section-specific visibility is no longer supported. See README for details.")
-          end
         end
       end
     end
